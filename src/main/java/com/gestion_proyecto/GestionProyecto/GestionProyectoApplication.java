9a17d4c9d0b217cf7896f41efbca6ccc3397f0ac
aortunoi <aortunoi@indra.es>
2025-02-19 16:52:00 +0100

    Primer commit

---
+++ src/main/java/com/gestion_proyecto/GestionProyecto/GestionProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.gestion_proyecto.GestionProyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion_proyecto/GestionProyecto/GestionProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion_proyecto.GestionProyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
